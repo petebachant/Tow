e8b38147aa6b653f7455db400974022b82e0d32f
Pete Bachant <petebachant@gmail.com>
2013-10-19 15:34:32 -0400

    Edit build script

--- buildrun.bat
+++ buildrun.bat
@@ -2,7 +2,7 @@ call buildgui
 
 cd ../pyinstaller-2.0
 
-python pyinstaller.py -y --icon=../Tow/icons/tow_icon_b.ico --noconsole ../PyQt-testing/Tow/tow.py
+python pyinstaller.py -y --icon=../Tow/icons/tow_icon_b.ico --noconsole ../Tow/tow.py
 
 cd Tow/dist/Tow
 
