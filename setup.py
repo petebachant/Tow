215bbe64e759b4d62acec6cdaaa3f27d789385ce
Pete Bachant <petebachant@gmail.com>
2015-10-11 13:04:33 -0400

    Revert "Use setuptools if available"
    This reverts commit 936bdc366b249cc43f25967d5cfe9ebfb3e2a474.
    
    `distutils` allows installation of data files in unzipped form, which is
    important if we want to use icons for the shortcut.

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 import os
 from tow import __version__
 
