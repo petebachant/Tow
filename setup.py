a9c4532e047b41b4f7ccc84518f154b33b13050f
Pete Bachant <petebachant@gmail.com>
2023-02-02 20:43:16 -0800

    Properly install package data

--- setup.py
+++ setup.py
@@ -5,19 +5,16 @@ from setuptools import setup
 import os
 from tow import __version__
 
-icons = os.listdir("tow/icons")
-icons = ["tow/icons/" + i for i in icons]
-
 setup(
-    name='Tow',
+    name="Tow",
     version=__version__,
-    author='Pete Bachant',
-    author_email='petebachant@gmail.com',
-    packages=['tow'],
-    scripts=['scripts/tow-script.py', 'scripts/tow.bat'],
-    data_files=[('Lib/site-packages/tow/icons', icons)],
-    url='https://github.com/petebachant/Tow.git',
-    license='LICENSE',
-    description='Python desktop app for controlling the UNH tow/wave tank tow carriage.',
-    long_description=open('README.md').read()
+    author="Pete Bachant",
+    author_email="petebachant@gmail.com",
+    packages=["tow"],
+    scripts=["scripts/tow-script.py", "scripts/tow.bat"],
+    include_package_data=True,
+    url="https://github.com/petebachant/Tow.git",
+    license="LICENSE",
+    description="Python desktop app for controlling the UNH tow/wave tank tow carriage.",
+    long_description=open("README.md").read(),
 )
